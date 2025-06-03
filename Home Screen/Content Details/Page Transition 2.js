8b3d1e428a5ca2713dcd49f3b843b703d10dd30e
Jeuel Manrique <jeuelmanrique8@gmail.com>
2025-06-03 21:52:04 +0800

    Delete Home Screen/Content Details/Page Transition 2.js

--- Home Screen/Content Details/Page Transition 2.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.upButton').addEventListener('click', () => {
-  const overlay = document.querySelector('.slideUp');
-  overlay.style.top = '0'; // Slide the overlay from bottom to top
-});
